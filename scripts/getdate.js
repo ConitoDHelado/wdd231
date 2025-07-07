de07ecdd8886a30a85065ca4c950b5a276f91314
ConitoDHelado <eprezedelgado@byupathway.edu>
2025-07-07 19:01:33 +0200

    added duck icon

--- scripts/getdate.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-
-currentyear.innerHTML = ` <span>${today.getFullYear()}</span>`;
-
-document.getElementById("lastModified").innerHTML = `<span>${document.lastModified}</span>`;
-
