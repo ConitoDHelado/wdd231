d4a715e6f93559ac35fcda576c3a089134991f80
ConitoDHelado <eprezedelgado@byupathway.edu>
2025-07-14 21:31:44 +0200

    weather activity

---
+++ scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=be42d46389c64c9c4b089942db709a32";
