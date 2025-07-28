33d8ac1310f1baa0c2343bf01a3a1fe908f88ab7
ConitoDHelado <eprezedelgado@byupathway.edu>
2025-07-28 23:24:28 +0200

    added message

--- chamber/scripts/lastVisit.js
+++ chamber/scripts/lastVisit.js
@@ -1,1 +1,29 @@
-console.log(localStorage.getItem('lastVisit'));
+function trackVisit() {
+    const lastVisit = localStorage.getItem('lastVisit');
+    const currentVisit = new Date().toISOString();
+    const message = document.querySelector('p');
+    
+    if (lastVisit) {
+        // User has visited before
+        const lastVisitDate = new Date(lastVisit);
+        const timeDifference = Date.now() - lastVisitDate.getTime();
+        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        if (daysDifference < 1) {
+            message.textContent = `Back so soon! Awesome!`;
+        }
+        else if (daysDifference === 1) {
+            message.textContent = `Welcome back! You last visited ${daysDifference} day ago.`;          
+        }
+
+    } else {
+        // First-time visitor
+        message.textContent = `Welcome! Let us know if you have any questions.`;
+        
+    }
+    
+    // Store current visit time
+    localStorage.setItem('lastVisit', currentVisit);
+}
+
+trackVisit();
