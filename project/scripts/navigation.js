c82487d8f15d2e592f9b237bc527ca7a2a05e0ab
ConitoDHelado <eprezedelgado@byupathway.edu>
2025-08-06 20:02:13 +0200

    added transition to nav

--- project/scripts/navigation.js
+++ project/scripts/navigation.js
@@ -3,5 +3,5 @@ const navBar = document.querySelector('#nav-bar');
 
 button.addEventListener('click', () => {
     button.classList.toggle('show');
-    navBar.classList.toggle('show');
+    navBar.classList.toggle('show')
 })
