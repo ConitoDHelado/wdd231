18905856787574d598725a4e75c5a8c654474147
ConitoDHelado <eprezedelgado@byupathway.edu>
2025-08-09 17:55:43 +0200

    finished home page

---
+++ project/scripts/get-sample-routes.js
@@ -0,0 +1,28 @@
+import {createCards} from "./create-cards.mjs"
+
+const url = 'https://conitodhelado.github.io/wdd231/project/data/routes.json'
+const section = document.querySelector('.routes')
+
+
+
+
+async function getRoutesData() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            const randomRoutes = data.sort(() => 0.5 - Math.random()).slice(0, 3);
+            createCards(randomRoutes, section);
+        } else {
+            throw Error(await response.text());
+        }
+
+    }
+    catch(error) {
+        console.log(error);
+    }
+    
+}
+
+getRoutesData();
+
